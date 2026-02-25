b1a1403c4cea7b57eb0969f31cf4b5362ccc064a
LauraG <naerys.404@gmail.com>
2026-02-25 15:36:59 +0100

    feat: GameService

--- src/main/java/com/adrar/cdafad/service/GameService.java
+++ src/main/java/com/adrar/cdafad/service/GameService.java
@@ -1,10 +1,14 @@
 package com.adrar.cdafad.service;
 
-import com.adrar.cdafad.entity.Category;
 import com.adrar.cdafad.entity.Game;
+import com.adrar.cdafad.entity.Manufacturer;
+import com.adrar.cdafad.exception.manufacturer.ManufacturerIsNotExistsException;
+import com.adrar.cdafad.exception.manufacturer.ManufacturerListIsEmptyException;
 import com.adrar.cdafad.repository.GameRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -20,6 +24,27 @@ public class GameService {
         }
 
         return gameRepository.save(game);
+    }
+
+    //Méthode pour récupérer un game
+    public Game getGameById(Integer id) throws Exception
+    {
+        Optional<Game> game = gameRepository.findById(id);
+        //Test si il n'existe pas
+        if (game.isEmpty()) {
+            throw new Exception("Le jeu n'existe pas.");
+        }
+        return game.get();
+    }
+
+    //Méthode pour récupérer la liste des games
+    public Iterable<Game> getAllGames() throws Exception
+    {
+        //Test si la liste est vide
+        if (this.gameRepository.count() == 0) {
+            throw new Exception("La liste de jeux est vide");
+        }
+        return gameRepository.findAll();
     }
 
 }
